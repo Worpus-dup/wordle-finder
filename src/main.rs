5cf7c28590e7f9a0688b0eae21fcce353867132a
Electro98 <geny_turov@mail.ru>
2026-08-26 13:29:15 +0900

    Set up basic app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let window = web_sys::window().expect("no global window");
+    let document = window.document().expect("no document");
+    let body = document.body().expect("no body");
+    body.set_inner_html("<h1>Wordle Finder</h1>");
 }
